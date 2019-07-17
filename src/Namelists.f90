98fab554fd196e7c89de57e3ab0736c7f0560c3e
rraynaud <raphael.raynaud@cea.fr>
2019-07-17 10:12:44 +0200

    Switch to compute the magnetic helicity
    mag_hel = <A,B>
    where A is the magnetic vector potential

--- src/Namelists.f90
+++ src/Namelists.f90
@@ -129,7 +129,7 @@ contains
       &    r_probe,theta_probe,n_phi_probes,n_probe_step,     &
       &    n_probe_out,t_probe_start,t_probe_stop,dt_probe,   &
       &    l_earth_likeness,l_max_comp,l_2D_spectra,l_2D_RMS, &
-      &    l_spec_avg
+      &    l_spec_avg,l_mag_hel
 
       namelist/mantle/conductance_ma,nRotMa,rho_ratio_ma, &
       &    omega_ma1,omegaOsz_ma1,tShift_ma1,             &
@@ -1116,6 +1116,7 @@ contains
       write(n_out,'(''  l_earth_likeness='',l3,'','')') l_earth_likeness
       write(n_out,'(''  l_max_comp      ='',i3,'','')') l_max_comp
       write(n_out,'(''  l_hel           ='',l3,'','')') l_hel
+      write(n_out,'(''  l_mag_hel       ='',l3,'','')') l_mag_hel
       write(n_out,'(''  l_AM            ='',l3,'','')') l_AM
       write(n_out,'(''  l_power         ='',l3,'','')') l_power
       write(n_out,'(''  l_viscBcCalc    ='',l3,'','')') l_viscBcCalc
@@ -1233,7 +1234,7 @@ contains
       anelastic_flavour="None" ! Useless in Boussinesq
       polo_flow_eq  ="WP"   ! Choose between 'DC' (double-curl) and 'WP' (Pressure)
       radial_scheme ="CHEB" ! Choose between 'CHEB' and 'FD'
-      time_scheme   ="CNAB2"   
+      time_scheme   ="CNAB2"
       mpi_transp    ="AUTO" ! automatic detection of the MPI strategy
 
       cacheblock_size_in_B=4096
@@ -1548,6 +1549,7 @@ contains
 
       !----- Different output, output times same as for log outout:
       l_hel         =.false. ! Helicity in misc.TAG
+      l_mag_hel     =.false. ! Magnetic helicity
       l_AM          =.false. ! Angular moment in AM.TAG
       l_power       =.false. ! power budget in power.TAG and dtE.TAG
       l_viscBcCalc  =.false. ! dissipation layer for stress-free BCs
